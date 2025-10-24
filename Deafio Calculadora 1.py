3808fb4f203e9bc7e05e943674f49dd354c22b97
sharkguimaraes-henrique-costa <shark06283@sharkcoders.pt>
2025-10-24 17:29:02 +0100

    Add files via upload

---
+++ Deafio Calculadora 1.py
@@ -0,0 +1,14 @@
+print("Calculadora de dois fatores")
+op=input("Qual é a operação que queres fazer?")
+pn=float(input("Qual é o primeiro número?"))
+sn=float(input("Qual é o segundo número?"))
+if op == "+":
+    valor = pn + sn
+elif op == "-":
+    valor = pn - sn
+elif op == "*":
+    valor = pn * sn
+elif op == "/":
+    valor = pn / sn
+
+
